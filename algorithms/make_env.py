1871212077d7867a591782f0d25a3bc2d864f7f7
karnikachoudhury <karnika.choudhury@gmail.com>
2026-02-28 13:12:07 -0600

    got regular PPO to run, looks stable enough

---
+++ algorithms/make_env.py
@@ -0,0 +1,19 @@
+import robosuite as suite
+from robosuite.wrappers import GymWrapper
+from stable_baselines3.common.monitor import Monitor
+
+def make_env():
+    env = suite.make(
+        env_name = "Lift",
+        robots = "Panda",
+        has_renderer = False,
+        use_camera_obs = False,
+        reward_shaping = True,
+        control_freq = 20,
+        horizon = 200,
+    )
+
+    env = GymWrapper(env)
+    env = Monitor(env)
+
+    return env
---
+++ algorithms/train_baseline.py
@@ -0,0 +1,21 @@
+from stable_baselines3 import PPO
+from make_env import make_env
+# uhh this is just vanilla PPO testing
+env = make_env()
+
+model = PPO("MlpPolicy", 
+            env, 
+            verbose = 1,
+            n_steps = 2048,
+            batch_size = 64,
+            gamma = 0.99,
+            gae_lambda = 0.95,
+            n_epochs = 10,
+            learning_rate = 3e-4,
+            clip_range = 0.2,
+            tensorboard_log = "./ppo_lift_tensorboard/"
+)
+model.learn(total_timesteps=300_000)
+model.save("ppo_lift_baseline")
+env.close()
+            
